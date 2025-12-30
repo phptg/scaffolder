52b9fd036aec6b458f8ce252abf75ed6994da0a0
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-30 22:52:54 +0300

    .editorconfig

--- src/changes.php
+++ src/changes.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
+use Vjik\Scaffolder\Change\CopyFile;
 use Vjik\Scaffolder\Change\NormalizeComposerJson;
 use Vjik\Scaffolder\Change\WriteLicense\Bsd3ClauseLicense;
 use Vjik\Scaffolder\Change\WriteLicense\WriteLicense;
+
+$files = dirname(__DIR__) . '/files';
 
 return [
     new WriteLicense(
         new Bsd3ClauseLicense('Sergei Predvoditelev'),
     ),
+    new CopyFile($files . '/.editorconfig', '.editorconfig'),
     new NormalizeComposerJson(),
 ];
