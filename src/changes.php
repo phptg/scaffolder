5338f6e8ca88279988a569b839ac8a728e6c3045
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-06-20 06:30:36 +0300

    zizmor audit (#9)

--- src/changes.php
+++ src/changes.php
@@ -155,6 +155,7 @@ return [
         ],
         UseInfection::class,
     ),
+    'dependabot.yaml' => new CopyFile($files . '/.github/dependabot.yaml', '.github/dependabot.yaml'),
     new CopyFile($files . '/.github/workflows/zizmor.yml', '.github/workflows/zizmor.yml'),
     new CopyFileIfNotExists($files . '/.gitattributes', '.gitattributes'),
 ];
